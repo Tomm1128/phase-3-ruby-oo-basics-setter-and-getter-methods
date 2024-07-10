733714b3fcedc5e5ce497ae1d060da2c00eb8bf9
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-10 09:39:30 -0400

    Refactor to do it the way the lab wanted

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,17 @@
 class Person
-  attr_accessor :name
-  attr_accessor :job
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+   def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
 end
